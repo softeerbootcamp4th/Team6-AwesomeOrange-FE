fea1a70cef72dfa2e37a6e802de3b25a963aac2e
lybell-art <futireflugel@gmail.com>
2024-07-23 16:39:06 +0900

    [feat] ErrorBoundary 컴포넌트 추가

--- src/main-client.jsx
+++ src/main-client.jsx
@@ -3,7 +3,8 @@ import { hydrateRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-hydrateRoot(document.getElementById("root"),
+hydrateRoot(
+  document.getElementById("root"),
   <StrictMode>
     <App />
   </StrictMode>,
