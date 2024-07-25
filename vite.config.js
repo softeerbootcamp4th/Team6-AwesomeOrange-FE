be7ec8527e492cb469e38368c3b2f930b69a6076
darkdulgi <daniel99285@gmail.com>
2024-07-25 17:50:45 +0900

    [fix] vite.config.js의 path를 base로 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path';
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  path:"./",
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
