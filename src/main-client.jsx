05481df8c7cbb66c70f4d2caa4e82dc4e832b31d
lybell-art <futireflugel@gmail.com>
2024-07-26 12:55:39 +0900

    [refactor] 개발 서버 CSR 기반으로 변경

--- src/main-client.jsx
+++ src/main-client.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-hydrateRoot(
-  document.getElementById("root"),
+const $root = document.getElementById("root");
+const app = (
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
+
+if (import.meta.env.DEV) {
+  const root = createRoot($root);
+  root.render(app);
+} else {
+  hydrateRoot($root, app);
+}
