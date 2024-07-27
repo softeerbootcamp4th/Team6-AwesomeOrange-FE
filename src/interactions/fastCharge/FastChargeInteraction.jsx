fa7d4fa4251903362549909e8929a69d494c1577
lybell <futireflugel@gmail.com>
2024-07-27 19:22:08 +0900

    [design] 현대 산스가 영문에만 적용되는 스타일 오류 수정

--- src/interactions/fastCharge/FastChargeInteraction.jsx
+++ src/interactions/fastCharge/FastChargeInteraction.jsx
@@ -15,10 +15,10 @@ function FastChargeInteraction({interacted, $ref})
 	useImperativeHandle($ref, ()=>({reset}), [setTimer]);
 
 	return <article className="bg-black w-full h-full overflow-hidden flex justify-center flex-col">
-		<div className="w-full max-w-[1200px] px-20 text-white">
-			<h3 className="text-neutral-400">불편함 없이, 더 빠르게</h3>
-			<p className="text-white">The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?</p>
-			<p className="text-white">다이얼을 돌려 충전에 필요한 시간을 확인해보세요!</p>
+		<div className="w-full max-w-[1200px] px-20 flex flex-col gap-3.5 font-bold">
+			<h3 className="text-neutral-400 text-title-l">불편함 없이, 더 빠르게</h3>
+			<p className="text-white text-body-l">The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?</p>
+			<p className="text-neutral-200 text-body-s">다이얼을 돌려 충전에 필요한 시간을 확인해보세요!</p>
 		</div>
 		<img src={dialSvg} alt="다이얼" className="w-48 h-48"></img>
 	</article>
