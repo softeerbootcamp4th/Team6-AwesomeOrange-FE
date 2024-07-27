f7d3f0a2087357353dda875afd6f87cec0f41167
lybell <futireflugel@gmail.com>
2024-07-27 17:31:08 +0900

    [feat] 고속충전 인터랙션 뼈대 추가(WIP)

---
+++ src/interactions/fastCharge/FastChargeInteraction.jsx
@@ -0,0 +1,27 @@
+import { useState, useImperativeHandle } from "react";
+import dialSvg from "./timer.svg";
+//import useDialDrag from "./useDialDrag.js";
+
+function FastChargeInteraction({interacted, $ref})
+{
+	const [timer, setTimer] = useState(0);
+	//const [dialRef, angle, resetAngle] = useDialDrag(0);
+
+	function reset()
+	{
+		setTimer(0);
+		//resetAngle();
+	}
+	useImperativeHandle($ref, ()=>({reset}), [setTimer]);
+
+	return <article className="bg-black w-full h-full overflow-hidden flex justify-center flex-col">
+		<div className="w-full max-w-[1200px] px-20 text-white">
+			<h3 className="text-neutral-400">불편함 없이, 더 빠르게</h3>
+			<p className="text-white">The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?</p>
+			<p className="text-white">다이얼을 돌려 충전에 필요한 시간을 확인해보세요!</p>
+		</div>
+		<img src={dialSvg} alt="다이얼" className="w-48 h-48"></img>
+	</article>
+}
+
+export default FastChargeInteraction;
