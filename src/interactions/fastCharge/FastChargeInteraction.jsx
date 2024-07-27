3e841d9f482a9cf79bca97c5c45033d1300869fb
lybell <futireflugel@gmail.com>
2024-07-27 20:01:32 +0900

    [design] 고속충전 인터랙션 배터리 부분, 하단 부분 반응형에 맞게 스타일링
    -[assets] 1,2,3,4,5 아이콘 추가

--- src/interactions/fastCharge/FastChargeInteraction.jsx
+++ src/interactions/fastCharge/FastChargeInteraction.jsx
@@ -1,4 +1,5 @@
 import { useState, useImperativeHandle } from "react";
+import orderIcon from "@/assets/property2.svg";
 import dialSvg from "./timer.svg";
 //import useDialDrag from "./useDialDrag.js";
 
@@ -14,13 +15,19 @@ function FastChargeInteraction({interacted, $ref})
 	}
 	useImperativeHandle($ref, ()=>({reset}), [setTimer]);
 
-	return <article className="bg-black w-full h-full overflow-hidden flex justify-center flex-col">
-		<div className="w-full max-w-[1200px] px-20 flex flex-col gap-3.5 font-bold">
-			<h3 className="text-neutral-400 text-title-l">불편함 없이, 더 빠르게</h3>
-			<p className="text-white text-body-l">The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?</p>
-			<p className="text-neutral-200 text-body-s">다이얼을 돌려 충전에 필요한 시간을 확인해보세요!</p>
+	return <article className="bg-black relative w-full h-full overflow-hidden flex items-center flex-col">
+		<div className="w-full max-w-[1200px] px-10 lg:px-20 flex gap-2 items-start mt-16 lg:mt-[6.25rem] ">
+			<img src={orderIcon} alt="2" />
+			<div className="flex flex-col gap-3.5 font-bold">
+				<h3 className="text-neutral-400 text-title-l">불편함 없이, 더 빠르게</h3>
+				<p className="text-white text-body-l">The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?</p>
+				<p className="text-neutral-200 text-body-s">다이얼을 돌려 충전에 필요한 시간을 확인해보세요!</p>
+			</div>
 		</div>
-		<img src={dialSvg} alt="다이얼" className="w-48 h-48"></img>
+		<div className="absolute top-[clamp(240px,40%,384px)] w-72 md:w-96 h-32 border-solid border-2 border-neutral-600 rounded-[30px]">
+			<div></div>
+		</div>
+		<img src={dialSvg} alt="다이얼" className="absolute bottom-0 size-72 md:size-96 lg:size-[562px] translate-y-1/2"></img>
 	</article>
 }
 
