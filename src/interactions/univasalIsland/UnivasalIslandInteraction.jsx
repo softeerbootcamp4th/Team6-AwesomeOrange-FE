d1981d06b88bc11f80237c431de442161ddc7e7e
lybell <futireflugel@gmail.com>
2024-07-28 17:04:59 +0900

    [design] 시트 위치 결정 (반응형으로)

--- src/interactions/univasalIsland/UnivasalIslandInteraction.jsx
+++ src/interactions/univasalIsland/UnivasalIslandInteraction.jsx
@@ -1,7 +1,19 @@
 import { useImperativeHandle } from "react";
 import orderIcon from "@/assets/property3.svg";
+import seat from "./assets/seat.svg";
+import univasalIsland from "./assets/univasalIsland.svg";
+import univasalIsland2 from "./assets/univasalIsland2.svg";
 
 function UnivasalIslandInteraction() {
+
+	const seatHullStyle = `absolute w-[1200px] h-[800px] 
+		bottom-[min(calc(100%-800px),-140px)] 
+		lg:bottom-[min(calc(100%-900px),-170px)] 
+		xl:bottom-[min(calc(100%-1000px),-200px)] 
+		-translate-x-24 md:translate-x-0
+		scale-[70%] lg:scale-[85%] xl:scale-100 
+	flex justify-center items-end origin-bottom`;
+
 	return (
 		<article className="relative w-full h-full overflow-hidden flex items-center flex-col">
 			<div className="w-full max-w-[1200px] px-10 lg:px-20 flex gap-2 items-start mt-16 lg:mt-[6.25rem] ">
@@ -17,6 +29,14 @@ function UnivasalIslandInteraction() {
 						유니버설 아일랜드를 드래그하여 이동시키고 스마트폰을 충전해보세요!
 					</p>
 				</div>
+			</div>
+			<div className={seatHullStyle}>
+				<img className="w-[453.48px] h-[718.4px]" src={seat} alt="left seat" />
+				<div className="w-[226px] h-[780px] flex flex-col gap-2">
+					<img src={univasalIsland} alt="univasal island" />
+					<img src={univasalIsland2} alt="univasal island" />
+				</div>
+				<img className="w-[453.48px] h-[718.4px]" src={seat} alt="right seat" />
 			</div>
 		</article>
 	)
