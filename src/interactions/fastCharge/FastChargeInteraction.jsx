0a7f921d5dca27c3023c75526cad921dcc606ede
lybell-art <futireflugel@gmail.com>
2024-07-29 14:20:01 +0900

    [refactor] 고속충전 인터랙션 컴포넌트에 인터랙션 설명 컴포넌트 적용

--- src/interactions/fastCharge/FastChargeInteraction.jsx
+++ src/interactions/fastCharge/FastChargeInteraction.jsx
@@ -1,6 +1,6 @@
 import { useImperativeHandle } from "react";
+import InteractionDescription from "../InteractionDescription.jsx";
 import BatteryProgressBar from "./BatteryProgressBar.jsx";
-import orderIcon from "@/assets/property2.svg";
 import dialSvg from "./timer.svg";
 import useDialDrag from "./useDialDrag.js";
 
@@ -35,20 +35,12 @@ function FastChargeInteraction({ interactCallback, $ref }) {
 
   return (
     <article className="relative w-full h-full overflow-hidden flex items-center flex-col">
-      <div className="w-full max-w-[1200px] px-10 lg:px-20 flex gap-2 items-start mt-16 lg:mt-[6.25rem] ">
-        <img src={orderIcon} alt="2" />
-        <div className="flex flex-col gap-3.5 font-bold">
-          <h3 className="text-neutral-400 text-title-m md:text-title-l">
-            불편함 없이, 더 빠르게
-          </h3>
-          <p className="text-white text-body-m md:text-body-l">
-            The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?
-          </p>
-          <p className="text-neutral-200 text-body-s">
-            다이얼을 돌려 충전에 필요한 시간을 확인해보세요!
-          </p>
-        </div>
-      </div>
+      <InteractionDescription 
+        order="2"
+        title="불편함 없이, 더 빠르게"
+        description="The new IONIQ 5의 배터리를 충전하는 데 얼마만큼의 시간이 걸릴까요?"
+        directive="다이얼을 돌려 충전에 필요한 시간을 확인해보세요!"
+      />
       <div className="absolute top-[clamp(240px,40%,384px)] w-72 md:w-96 h-32 border-solid border-2 border-neutral-600 rounded-[30px] p-3.5">
         <div className="absolute w-5 h-9 bg-neutral-600 right-[-1.25rem] top-[2.875rem] rounded-r-md"></div>
         <BatteryProgressBar progress={progress} />
