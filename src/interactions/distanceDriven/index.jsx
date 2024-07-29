64856056773e19abe3e23b200eb3fc42d10adf70
lybell-art <futireflugel@gmail.com>
2024-07-29 14:45:24 +0900

    [design] 하단부 스타일 추가

--- src/interactions/distanceDriven/index.jsx
+++ src/interactions/distanceDriven/index.jsx
@@ -9,6 +9,15 @@ function DistanceDrivenInteraction()
 			description="The new IONIQ 5는 한 번의 충전으로 얼마나 멀리 주행할 수 있을까요?"
 			directive="가운데 점을 드래그하여 최대 주행거리를 예측해보세요!"
 		/>
+		<div className="absolute top-1/2">
+			<div className="rounded-full size-8 bg-blue-300 cursor-pointer"></div>
+		</div>
+		<p className="text-white absolute bottom-32 md:bottom-36 lg:bottom-[180px] text-title-s pointer-events-none">
+			<span className="text-head-m md:text-head-l lg:text-[4.375rem] mr-1.5 lg:mr-2.5">
+				{0}
+			</span>
+			km
+		</p>
 	</article>
 }
 
