a64f739a567bf50cabc69eca55829d3185f46860
lybell-art <futireflugel@gmail.com>
2024-07-29 18:00:06 +0900

    [chore] 린트 수정

--- src/interactions/subsidy/index.jsx
+++ src/interactions/subsidy/index.jsx
@@ -8,26 +8,29 @@ function SubsidyInteraction({ interactCallback, $ref }) {
   const [count, setCount] = useState(0);
   const rottieRef = useRef(null);
 
-  function onClick(e)
-  {
-  	setCount( count=>count+1 );
-  	e.currentTarget.animate(
-      [
-        { transform: "rotateY(0)" },
-        { transform: "rotateY(360deg)" },
-      ],
+  function onClick(e) {
+    setCount((count) => count + 1);
+    e.currentTarget.animate(
+      [{ transform: "rotateY(0)" }, { transform: "rotateY(360deg)" }],
       {
         duration: 500,
         iteractions: 1,
         easing: "cubic-bezier(0.215, 0.610, 0.355, 1.000)", // ease-out-cubic
       },
     );
-  	rottieRef.current.goToAndPlay(0);
+    rottieRef.current.goToAndPlay(0);
+    interactCallback?.();
   }
 
-  useImperativeHandle($ref, () => ({ reset(){
-  	setCount(0);  
-  } }), []);
+  useImperativeHandle(
+    $ref,
+    () => ({
+      reset() {
+        setCount(0);
+      },
+    }),
+    [],
+  );
 
   return (
     <article className="relative w-full h-full overflow-hidden flex items-center flex-col">
@@ -37,11 +40,25 @@ function SubsidyInteraction({ interactCallback, $ref }) {
         description="The new IONIQ 5는 한 번의 충전으로 얼마나 멀리 주행할 수 있을까요?"
         directive="가운데 점을 드래그하여 최대 주행거리를 예측해보세요!"
       />
-      <div className="absolute z-0 w-96 h-96 top-[calc(50%-12rem)] flex justify-center items-center" >
-      	<div className="absolute size-[120px] rounded-full bg-blue-400 flex justify-center items-center active:scale-90 transition-transform" onClick={onClick}>
-      		<img src={dollor} className="select-none" alt="$" width="22.8" height="35.84" />
-      	</div>
-      	<Lottie className="absolute -z-10" animationData={coinRottie} ref={rottieRef} loop={false}/>
+      <div className="absolute z-0 w-96 h-96 top-[calc(50%-12rem)] flex justify-center items-center">
+        <div
+          className="absolute size-[120px] rounded-full bg-blue-400 flex justify-center items-center active:scale-90 transition-transform"
+          onClick={onClick}
+        >
+          <img
+            src={dollor}
+            className="select-none"
+            alt="$"
+            width="22.8"
+            height="35.84"
+          />
+        </div>
+        <Lottie
+          className="absolute -z-10"
+          animationData={coinRottie}
+          ref={rottieRef}
+          loop={false}
+        />
       </div>
       <p className="text-white absolute bottom-32 md:bottom-36 lg:bottom-[180px] text-title-s pointer-events-none">
         <span className="text-head-m md:text-head-l lg:text-[4.375rem] mr-1.5 lg:mr-2.5">
