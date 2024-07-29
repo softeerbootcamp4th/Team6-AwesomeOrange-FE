2c65716646f1bbc39f3768ec5a2096459d609d1d
lybell-art <futireflugel@gmail.com>
2024-07-29 17:09:09 +0900

    [feat] 보조금 인터랙션 기본동작 추가

---
+++ src/interactions/subsidy/index.jsx
@@ -0,0 +1,38 @@
+import { useState, useImperativeHandle } from "react";
+import dollor from "./assets/dollor.svg";
+import InteractionDescription from "../InteractionDescription.jsx";
+
+function SubsidyInteraction({ interactCallback, $ref }) {
+  const [count, setCount] = useState(0);
+
+  function onClick()
+  {
+  	setCount( count=>count+1 );
+  }
+
+  useImperativeHandle($ref, () => ({ reset(){
+  	setCount(0);
+  } }), []);
+
+  return (
+    <article className="relative w-full h-full overflow-hidden flex items-center flex-col">
+      <InteractionDescription
+        order="5"
+        title="걱정 없이, 더 멀리"
+        description="The new IONIQ 5는 한 번의 충전으로 얼마나 멀리 주행할 수 있을까요?"
+        directive="가운데 점을 드래그하여 최대 주행거리를 예측해보세요!"
+      />
+      <div className="absolute top-1/2 size-[120px] rounded-full bg-blue-400 flex justify-center items-center" onClick={onClick}>
+      	<img src={dollor} className="select-none" alt="$" width="22.8" height="35.84" />
+      </div>
+      <p className="text-white absolute bottom-32 md:bottom-36 lg:bottom-[180px] text-title-s pointer-events-none">
+        <span className="text-head-m md:text-head-l lg:text-[4.375rem] mr-1.5 lg:mr-2.5">
+          {count * 10}
+        </span>
+        만원
+      </p>
+    </article>
+  );
+}
+
+export default SubsidyInteraction;
