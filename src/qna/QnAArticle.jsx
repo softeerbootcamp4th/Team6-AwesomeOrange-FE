fd35d102b04e04161f19728dd5612e475ba22b7e
lybell-art <futireflugel@gmail.com>
2024-07-31 15:19:40 +0900

    [feat] QnA 아이콘 회전 애니메이션 구현

--- src/qna/QnAArticle.jsx
+++ src/qna/QnAArticle.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import arrow from "./assets/arrow.svg";
 
 function QnAArticle({question, answer})
 {
@@ -20,7 +21,8 @@ function QnAArticle({question, answer})
 	}
 	const staticArticleStyle = `text-neutral-400 text-justify relative
 	before:w-full before:h-full before-block before:absolute before:bg-white before:pointer-events-none
-	before:scale-y-0 before:transform-translate before:duration-200 before:ease-linear before:origin-bottom`;
+	before:scale-y-0 before:transition-transform before:duration-200 before:ease-linear before:origin-bottom`;
+	const staticIconStyle = "size-10 transition-transform ease-in-out-cubic";
 
 	return <article className="w-full max-w-[1200px] flex flex-col gap-8 py-6 text-body-s md:text-body-m lg:text-body-l">
 		<div className="flex justify-between">
@@ -28,7 +30,11 @@ function QnAArticle({question, answer})
 				<span className="text-blue-400">Q.</span>
 				{question}
 			</h3>
-			<div onClick={onClick}>열기/닫기</div>
+			<img className={`${staticIconStyle} ${opened ? "rotate-0" : "rotate-180"}`}
+			src={arrow}
+			alt={opened ? "닫기" : "열기"}
+			onClick={onClick}
+			draggable="false"/>
 		</div>
 		<p className={`${staticArticleStyle} ${opened ? "before:scale-y-0" : "before:scale-y-100"} ${visible ? "block" : "hidden"}`}>{answer}</p>
 	</article>
