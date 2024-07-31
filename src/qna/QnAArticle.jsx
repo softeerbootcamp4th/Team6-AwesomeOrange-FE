3476ad1f911b2041cba7529e9e0f7dd9c3d1005a
lybell-art <futireflugel@gmail.com>
2024-07-31 15:32:38 +0900

    [design] QnA 제목 추가, 실제 컨텐츠 추가

--- src/qna/QnAArticle.jsx
+++ src/qna/QnAArticle.jsx
@@ -22,18 +22,17 @@ function QnAArticle({question, answer})
 	const staticArticleStyle = `text-neutral-400 text-justify relative
 	before:w-full before:h-full before-block before:absolute before:bg-white before:pointer-events-none
 	before:scale-y-0 before:transition-transform before:duration-200 before:ease-linear before:origin-bottom`;
-	const staticIconStyle = "size-10 transition-transform ease-in-out-cubic";
+	const staticIconStyle = "size-[1.8181818em] transition-transform ease-in-out-cubic";
 
-	return <article className="w-full max-w-[1200px] flex flex-col gap-8 py-6 text-body-s md:text-body-m lg:text-body-l">
-		<div className="flex justify-between">
-			<h3 className="flex gap-3 font-bold text-black">
+	return <article className="w-full max-w-[1200px] flex flex-col gap-4 md:gap-6 lg:gap-8 py-6 text-body-s md:text-body-m lg:text-body-l">
+		<div className="flex justify-between items-center" onClick={onClick}>
+			<h3 className="flex gap-2 lg:gap-3 font-bold text-black">
 				<span className="text-blue-400">Q.</span>
 				{question}
 			</h3>
 			<img className={`${staticIconStyle} ${opened ? "rotate-0" : "rotate-180"}`}
 			src={arrow}
 			alt={opened ? "닫기" : "열기"}
-			onClick={onClick}
 			draggable="false"/>
 		</div>
 		<p className={`${staticArticleStyle} ${opened ? "before:scale-y-0" : "before:scale-y-100"} ${visible ? "block" : "hidden"}`}>{answer}</p>
--- src/qna/index.jsx
+++ src/qna/index.jsx
@@ -1,12 +1,11 @@
 import QnAArticle from "./QnAArticle.jsx";
-//import content from "./content.json";
-
-const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+import content from "./content.json";
 
 function QnASection()
 {
-	return <section className="w-full px-4 py-[7.5rem] flex flex-col items-center">
-		<QnAArticle question="hello, world!" answer={lorem} />
+	return <section className="w-full px-4 py-20 lg:py-[7.5rem] flex flex-col items-center">
+		<h2 className="text-head-s lg:text-head-m">자주 묻는 질문</h2>
+		{content.map( (item)=><QnAArticle key={item.question} {...item} /> )}
 	</section>
 }
 
