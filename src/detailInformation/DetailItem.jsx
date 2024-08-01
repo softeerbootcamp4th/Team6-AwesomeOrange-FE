a6c14309832ad3c52d0ad58405e9632d42633831
lybell-art <futireflugel@gmail.com>
2024-08-01 15:24:13 +0900

    [design] 반응형 디자인에 맞게 스와이퍼 내용 수정, 스와이퍼 내용 데이터 기반으로 추가

---
+++ src/detailInformation/DetailItem.jsx
@@ -0,0 +1,12 @@
+function DetailItem({img, title, description})
+{
+	return <article className="w-full h-full p-12 relative flex flex-col justify-end">
+		<img src={img} alt={title} className="absolute w-full h-full -z-10 top-0 left-0 object-cover" width="1920" height="1080"/>
+		<div className="text-white w-full flex flex-col gap-4">
+			<h3 className="whitespace-pre-wrap text-title-s md:text-title-m lg:text-title-l">{title}</h3>
+			<p className="whitespace-pre-wrap text-body-s md:text-body-m lg:text-body-l">{description}</p>
+		</div>
+	</article>;
+}
+
+export default DetailItem;
--- src/detailInformation/index.jsx
+++ src/detailInformation/index.jsx
@@ -1,45 +1,54 @@
 import useSwiperState from "@/common/useSwiperState.js";
+import DetailItem from "./DetailItem.jsx";
+import content from "./content.json";
 import left from "./assets/left.svg";
 import right from "./assets/right.svg";
-
-const PAGE_COUNT = 3;
 
 function DetailInformation()
 {
 	const [page, swiperElRef] = useSwiperState();
-	const isLastPage = page === PAGE_COUNT-1;
+	const isLastPage = page === content.length-1;
 
-	const slideClass = "w-full max-w-[1200px] bg-yellow-400";
-	const navigationClass = `absolute [--size:3rem] md:[--size:4.5rem] top-[calc(50%-var(--size)*0.5)] size-[var(--size)] p-2 md:p-4 
+	const slideClass = "w-[calc(100%-128px)] min-[1024px]:w-full max-w-[1200px] bg-yellow-400";
+	const navigationClass = `invisible absolute [--size:3rem] md:[--size:4.5rem] top-[calc(50%-var(--size)*0.5)] size-[var(--size)] p-2 md:p-4 
 	flex justify-center items-center rounded-full bg-neutral-100 z-10 cursor-pointer`;
+
+	const breakpoints = JSON.stringify(
+	{
+		1024: {
+			spaceBetween: 400
+		}
+	});
 
 	return <section className="w-full flex flex-col items-center gap-10">
 		<div className="w-full max-w-[1728px] h-[32rem] lg:h-[50rem] relative">
 			<swiper-container class="w-full h-full" 
-				slides-per-view="auto" space-between="400" centered-slides="true"
+				slides-per-view="auto" centered-slides="true"
+				space-between="20"
+				breakpoints={breakpoints}
 				ref={swiperElRef} 
 			>
-				<swiper-slide class={slideClass}>Hi</swiper-slide>
-				<swiper-slide class={slideClass}>I'm</swiper-slide>
-				<swiper-slide class={slideClass}>Temmie</swiper-slide>
+				{ content.map( (item)=><swiper-slide class={slideClass} key={item.title}>
+					<DetailItem {...item} />
+				</swiper-slide> ) }
 			</swiper-container>
-			<div className={`${navigationClass} left-6 ${page === 0 ? "invisible" : ""}`} onClick={ ()=>{
+			<div className={`${navigationClass} left-6 ${page === 0 ? "" : "lg:visible"}`} onClick={ ()=>{
 				swiperElRef.current.swiper.slidePrev();
 			} }>
 				<img src={left} alt="left" width="40" height="40" />
 			</div>
-			<div className={`${navigationClass} right-6 ${isLastPage ? "invisible" : ""}`} onClick={ ()=>{
+			<div className={`${navigationClass} right-6 ${isLastPage ? "" : "lg:visible"}`} onClick={ ()=>{
 				swiperElRef.current.swiper.slideNext();
 			} }>
 				<img src={right} alt="right" width="40" height="40" />
 			</div>
 		</div>
-		<div className="w-full max-w-[1248px] px-6 h-14 flex justify-end lg:justify-between align-center">
-			<div className="hidden lg:flex">
-				<div>어쩌구</div>
-				<div>저쩌구</div>
-				<div>거쩌구</div>
-			</div>
+		<div className="w-full max-w-[1248px] px-16 lg:px-6 h-14 flex justify-end lg:justify-between align-center">
+			<ul className="hidden lg:flex text-body-l">
+				{ content.map( ({tabName}, i)=><li className={i === page ? "text-black" : "text-neutral-300"} key={tabName} onClick={ ()=>{
+					swiperElRef.current.swiper.slideTo(i);
+				} }>{tabName}</li> ) }
+			</ul>
 			<div className="flex justify-end flex-wrap gap-6 h-full">
 				<a href="https://www.hyundai.com/kr/ko/e/vehicles/the-new-ioniq5/intro" target="_blank" className="text-body-s border-black border-2 bg-white text-black px-6 py-4">
 					더뉴 아이오닉 5 더 알아보기
