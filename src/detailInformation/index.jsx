c52abc24f67c8424036feaaec5164ac3f2dee6d6
lybell-art <futireflugel@gmail.com>
2024-08-01 16:15:25 +0900

    [feat] 차량 상세 정보 타이틀 추가

--- src/detailInformation/index.jsx
+++ src/detailInformation/index.jsx
@@ -1,85 +1,15 @@
-import useSwiperState from "@/common/useSwiperState.js";
-import DetailItem from "./DetailItem.jsx";
+import DetailSwiper from "./DetailSwiper.jsx";
 import content from "./content.json";
-import left from "./assets/left.svg";
-import right from "./assets/right.svg";
 
-function DetailInformation() {
-  const [page, swiperElRef] = useSwiperState();
-  const isLastPage = page === content.length - 1;
-
-  const slideClass =
-    "w-[calc(100%-96px)] min-[1024px]:w-full max-w-[1200px] bg-yellow-400";
-  const navigationClass = `invisible absolute [--size:3rem] md:[--size:4.5rem] top-[calc(50%-var(--size)*0.5)] size-[var(--size)] p-2 md:p-4 
-	flex justify-center items-center rounded-full bg-neutral-100 z-10 cursor-pointer`;
-
-  return (
-    <section className="w-full flex flex-col items-center gap-10">
-      <div className="w-full max-w-[1728px] h-[32rem] lg:h-[50rem] relative">
-        <swiper-container
-          class="w-full h-full"
-          slides-per-view="auto"
-          centered-slides="true"
-          space-between="12"
-          breakpoints='{"1024":{"spaceBetween":400}}'
-          ref={swiperElRef}
-        >
-          {content.map((item) => (
-            <swiper-slide class={slideClass} key={item.title}>
-              <DetailItem {...item} />
-            </swiper-slide>
-          ))}
-        </swiper-container>
-        <div
-          className={`${navigationClass} left-6 ${page === 0 ? "" : "lg:visible"}`}
-          onClick={() => {
-            swiperElRef.current.swiper.slidePrev();
-          }}
-        >
-          <img src={left} alt="left" width="40" height="40" />
-        </div>
-        <div
-          className={`${navigationClass} right-6 ${isLastPage ? "" : "lg:visible"}`}
-          onClick={() => {
-            swiperElRef.current.swiper.slideNext();
-          }}
-        >
-          <img src={right} alt="right" width="40" height="40" />
-        </div>
-      </div>
-      <div className="w-full max-w-[1248px] px-12 lg:px-6 h-14 flex justify-end lg:justify-between align-center font-bold">
-        <ul className="hidden lg:flex text-body-l gap-[3.75rem]">
-          {content.map(({ tabName }, i) => (
-            <li
-              className={i === page ? "text-black" : "text-neutral-300"}
-              key={tabName}
-              onClick={() => {
-                swiperElRef.current.swiper.slideTo(i);
-              }}
-            >
-              {tabName}
-            </li>
-          ))}
-        </ul>
-        <div className="flex justify-end flex-wrap gap-6 h-full">
-          <a
-            href="https://www.hyundai.com/kr/ko/e/vehicles/the-new-ioniq5/intro"
-            target="_blank"
-            className="text-body-s border-black border-2 bg-white text-black px-6 py-4"
-          >
-            더뉴 아이오닉 5 더 알아보기
-          </a>
-          <a
-            href="https://www.hyundai.com/kr/ko/e/vehicles/purchase-consult"
-            target="_blank"
-            className="text-body-s bg-black text-white px-6 py-4"
-          >
-            구매 상담 신청
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+function DetailInformation()
+{
+	return <section className="w-full flex flex-col items-center py-24 lg:py-60 gap-16 lg:gap-40">
+		<div className="flex flex-col gap-3 lg:gap-9 text-center font-bold items-center">
+			<p className="text-body-m text-neutral-600 w-fit py-3 lg:py-5 border-b-[3px] border-blue-300">차량 상세 정보</p>
+			<h2 className="text-head-s lg:text-head-m text-black">The new IONIQ 5<br/>전기차의 <span className="sketch-line">ICON</span>이 되다.</h2>
+		</div>
+		<DetailSwiper content={content}/>
+	</section>
 }
 
 export default DetailInformation;
