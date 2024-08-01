60cd5f8e62a402ce719e92b628fa742d95476110
lybell-art <futireflugel@gmail.com>
2024-08-01 13:56:30 +0900

    [feat] 버튼으로 스와이프하는 기능 추가

--- src/detailInformation/index.jsx
+++ src/detailInformation/index.jsx
@@ -1,13 +1,39 @@
+import useSwiperState from "@/common/useSwiperState.js";
+
+const PAGE_COUNT = 3;
+
 function DetailInformation()
 {
+	const [page, swiperElRef] = useSwiperState();
+	const isLastPage = page === PAGE_COUNT-1;
+
 	const slideClass = "w-full max-w-[1200px] bg-yellow-400";
+	const navigationClass = "absolute top-[calc(50%-2.25rem)] size-[4.5rem] p-4 rounded-full bg-neutral-100 z-10 cursor-pointer";
 
 	return <section className="w-full flex flex-col items-center">
-		<swiper-container class="w-full max-w-[2000px] h-[32rem] lg:h-[50rem]" slides-per-view="auto" space-between="400" centered-slides="true">
-			<swiper-slide class={slideClass}>Hi</swiper-slide>
-			<swiper-slide class={slideClass}>I'm</swiper-slide>
-			<swiper-slide class={slideClass}>Temmie</swiper-slide>
-		</swiper-container>
+		<div className="w-full max-w-[2000px] h-[32rem] lg:h-[50rem] relative">
+			<swiper-container class="w-full h-full" 
+				slides-per-view="auto" space-between="400" centered-slides="true"
+				ref={swiperElRef} 
+			>
+				<swiper-slide class={slideClass}>Hi</swiper-slide>
+				<swiper-slide class={slideClass}>I'm</swiper-slide>
+				<swiper-slide class={slideClass}>Temmie</swiper-slide>
+			</swiper-container>
+			<div className={`${navigationClass} left-0 ${page === 0 ? "invisible" : ""}`} onClick={ ()=>{
+				swiperElRef.current.swiper.slidePrev();
+			} }>
+				Left
+			</div>
+			<div className={`${navigationClass} right-0 ${isLastPage ? "invisible" : ""}`} onClick={ ()=>{
+				swiperElRef.current.swiper.slideNext();
+			} }>
+				Right
+			</div>
+			<div className="absolute top-0 right-24 z-10 cursor-pointer">
+				{page}
+			</div>
+		</div>
 	</section>
 }
 
