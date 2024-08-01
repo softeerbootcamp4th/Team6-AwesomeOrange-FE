c3c0bbb42d552ac5d1388f51833bb7179ddf074e
lybell-art <futireflugel@gmail.com>
2024-08-01 11:50:17 +0900

    [design] 스와이퍼 크기 스타일 추가

--- src/detailInformation/index.jsx
+++ src/detailInformation/index.jsx
@@ -1,10 +1,12 @@
 function DetailInformation()
 {
+	const slideClass = "w-full max-w-[1200px] bg-yellow-400";
+
 	return <section className="w-full flex flex-col items-center">
-		<swiper-container class="w-full">
-			<swiper-slide>Hi</swiper-slide>
-			<swiper-slide>I'm</swiper-slide>
-			<swiper-slide>Temmie</swiper-slide>
+		<swiper-container class="w-full max-w-[2000px] h-[32rem] lg:h-[50rem]" slides-per-view="auto" space-between="400" centered-slides="true">
+			<swiper-slide class={slideClass}>Hi</swiper-slide>
+			<swiper-slide class={slideClass}>I'm</swiper-slide>
+			<swiper-slide class={slideClass}>Temmie</swiper-slide>
 		</swiper-container>
 	</section>
 }
