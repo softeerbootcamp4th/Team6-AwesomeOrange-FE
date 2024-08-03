911d93da8c159a7e2946daf5bd59bc3aefa2e223
lybell <futireflugel@gmail.com>
2024-08-03 17:19:48 +0900

    [feat] 기대평 에러 컴포넌트 추가

--- src/comment/CommentCarouselError.jsx
+++ src/comment/CommentCarouselError.jsx
@@ -1,8 +1,9 @@
 function CommentCarouselError()
 {
 	return <div className="w-full h-[29rem] flex justify-center items-center px-6">
-		<div className="w-full max-w-[1200px] h-96 bg-neutral-50 flex justify-center items-center">
-			<p className="text-body-s md:text-body-m lg:text-body-l text-red-500">기대평 정보를 불러오지 못했어요!</p>
+		<div className="w-full max-w-[1200px] h-96 bg-neutral-50 flex flex-col justify-center items-center gap-4">
+			<img src="/icons/error.svg" alt="불러오기 오류" width="120" height="120" />
+			<p className="text-body-l text-red-500 font-bold">기대평 정보를 불러오지 못했어요!</p>
 		</div>
 	</div>
 }
