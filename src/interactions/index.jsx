30e83159a3359fd0b394e2e60b4f887cb7b4789b
darkdulgi <daniel99285@gmail.com>
2024-08-06 18:44:05 +0900

    [feat] swiper 기초 세팅

--- src/interactions/index.jsx
+++ src/interactions/index.jsx
@@ -3,7 +3,11 @@ import useSectionInitialize from "../scroll/useSectionInitialize";
 import IntroductionDetail from "./IntroductionDetail";
 import GiftDetail from "./GiftDetail";
 import JSONData from "./content.json";
-import TarBar from "./TabBar";
+import TabBar from "./TabBar";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 export default function InteractionPage() {
   const SECTION_IDX = 1;
@@ -18,15 +22,32 @@ export default function InteractionPage() {
       ref={sectionRef}
       className="bg-black py-60 flex flex-col items-center"
     >
-      <TarBar
+      <TabBar
         currentInteraction={currentInteraction}
         setCurrentInteraction={setCurrentInteraction}
         isJoinedList={isJoinedList}
       />
 
-      <div className="h-[456px] w-[500px] border border-white flex justify-center items-center text-white">
-        인터랙션
-      </div>
+      <Swiper
+        slidesPerView={"auto"}
+        centeredSlides={true}
+        spaceBetween={15}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="w-full bg-white h-full"
+      >
+        <SwiperSlide className="w-1/3 h-[500px]">
+          <div className="bg-red-400">1</div>
+        </SwiperSlide>
+        <SwiperSlide className="w-1/3 h-[500px]">
+          <div className="bg-red-500">2</div>
+        </SwiperSlide>
+        <SwiperSlide className="w-1/3 h-[500px]">
+          <div className="bg-red-400">3</div>
+        </SwiperSlide>
+      </Swiper>
 
       <div className="pt-32 flex flex-col xl:flex-row gap-[140px]">
         <IntroductionDetail contentList={JSONData.howto} />
