d31bdacb5f18d4caefd5c172ed4454d0bb521640
lybell-art <futireflugel@gmail.com>
2024-08-09 13:36:24 +0900

    [feat] 하단 선착순 이벤트 notifier가 카운트를 보여주도록 수정

--- src/fcfs/mock.js
+++ src/fcfs/mock.js
@@ -9,6 +9,15 @@ const handlers = [
   http.get("/api/serverTime", () => {
     return HttpResponse.json({ timestamp: "2024-08-08T06:00:00.500+00:00" });
   }),
+  http.get("/api/v1/event/fcfs/:eventFrameId/info", () => {
+    return HttpResponse.json({
+      nowDateTime: "2024-08-08T06:00:25.000Z",
+      eventStatus: "progress",
+    });
+  }),
+  http.get("/api/v1/event/fcfs/participated", () => {
+    return HttpResponse.json({ answerResult: false, winner: false });
+  }),
   http.post("/api/v1/event/fcfs/:eventFrameId", async ({request}) => {
     const { eventAnswer } = await request.json();
 
@@ -23,15 +32,6 @@ const handlers = [
     if(typeof eventAnswer !== "number") return HttpResponse.json(false, { status: 400 });
 
     return HttpResponse.json({answerResult, winner});
-  }),
-  http.get("/api/v1/event/fcfs/:eventFrameId/info", () => {
-    return HttpResponse.json({
-      nowDateTime: "2024-08-08T05:00:02.000Z",
-      eventStatus: "progress",
-    });
-  }),
-  http.get("/api/v1/event/fcfs/participated", () => {
-    return HttpResponse.json({ answerResult: false, winner: false });
   }),
 ];
 
---
+++ src/introSection/notifier/index.jsx
@@ -0,0 +1,28 @@
+import FcfsNotifierCountdown from "./FcfsNotifierCountdown.jsx";
+import scrollTo from "@/scroll/scrollTo.js";
+import useFcfsStore from "@/fcfs/store.js";
+import { PROGRESS, OFFLINE } from "@/fcfs/constants.js";
+import { FCFS_SECTION } from "@/common/constants.js";
+
+function FcfsNotifier({visible})
+{
+  const eventStatus = useFcfsStore( store=>store.eventStatus );
+
+  function onClick()
+  {
+    scrollTo(FCFS_SECTION);
+  }
+
+  if(eventStatus === OFFLINE) return null;
+
+  return <button
+    onClick={onClick}
+    className={`${visible ? "-translate-y-24" : ""} -bottom-20 transition duration-150 ease-in-out fixed left-1/2 -translate-x-1/2 graphic-gradient rounded-full p-px shadow-[0_4px_12px_0px_rgba(0,0,0,0.25)] z-40 select-none`}
+  >
+    <div className="bg-black flex items-center gap-[10px] px-10 py-4 rounded-full text-body-m font-bold">
+      {eventStatus === PROGRESS ? <span className="text-transparent bg-clip-text graphic-gradient">현재 이벤트 진행중!</span> : <FcfsNotifierCountdown />}
+    </div>
+  </button>
+}
+
+export default FcfsNotifier;
