a9f8afaa2c6bc5e2dbbbf6cd66f29c9abd3f5a22
lybell <futireflugel@gmail.com>
2024-08-11 08:52:07 +0900

    [design] 이벤트 상세안내 설명 그리드로 전환

--- src/fcfs/description/index.jsx
+++ src/fcfs/description/index.jsx
@@ -4,7 +4,7 @@ import content from "./content.json";
 
 function FcfsDescription() {
   return (
-    <div className="w-full md:w-[640px] lg:w-full max-w-[1200px] flex flex-col justify-between lg:flex-row gap-16">
+    <div className="w-full md:w-[640px] lg:w-full max-w-[1200px] flex flex-col justify-between lg:flex-row gap-16 lg:gap-10 xl:gap-16">
       <EventDetail {...content.detail} />
       <div className="flex flex-col gap-10 w-full lg:w-1/2 lg:max-w-[510px]">
         <h4 className="text-body-l font-bold text-white">경품 안내</h4>
