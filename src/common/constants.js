edef6ea36f9ee4c1aa6df96bde58adeb55e5bf44
lybell-art <futireflugel@gmail.com>
2024-08-12 10:56:35 +0900

    [refactor] 폴더 구조 정리

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,8 +1,2 @@
 export const EVENT_ID = "0";
 export const TOKEN_ID = "AWESOME_ORANGE_ACCESS_TOKEN";
-
-// scroll section constants
-export const INTERACTION_SECTION = 1;
-export const DETAIL_SECTION = 2;
-export const COMMENT_SECTION = 3;
-export const FCFS_SECTION = 4;
---
+++ src/mainPage/shared/scroll/constants.js
@@ -0,0 +1,5 @@
+// scroll section constants
+export const INTERACTION_SECTION = 1;
+export const DETAIL_SECTION = 2;
+export const COMMENT_SECTION = 3;
+export const FCFS_SECTION = 4;
