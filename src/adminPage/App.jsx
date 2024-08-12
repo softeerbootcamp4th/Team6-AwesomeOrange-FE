8bb25282f851d86c2ca87051fbcfd4036db3c0b7
lybell-art <futireflugel@gmail.com>
2024-08-12 18:17:06 +0900

    [scaffold] 라우팅 세팅

--- src/adminPage/App.jsx
+++ src/adminPage/App.jsx
@@ -4,7 +4,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/events" element={<div>events</div>} />
+        <Route exact path="/events/create" element={<div>event 생성 화면</div>} />
+        <Route path="/events/:id" element={<div>event 보는 화면</div>} />
+        <Route path="/events" element={<div>이벤트 목록 화면</div>} />
+        <Route path="/comments/:id" element={<div>기대평 화면</div>} />
+        <Route path="/comments" element={<div>기대평 검색 화면</div>} />
+        <Route path="/login" element={<div>로그인 화면</div>} />
         <Route path="/" element={<div>hello</div>} />
       </Routes>
     </>
