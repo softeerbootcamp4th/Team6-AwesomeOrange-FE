3dda0470b4859536d8b90618cc633b21cc96aebb
lybell-art <futireflugel@gmail.com>
2024-08-12 16:52:43 +0900

    [build] 어드민 페이지와 메인페이지 다르게 빌드되게 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import svgr from "vite-plugin-svgr";
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
