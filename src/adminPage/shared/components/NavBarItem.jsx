e06a2967823ccadf2745e41156076b93b6d32287
lybell-art <futireflugel@gmail.com>
2024-08-13 15:01:50 +0900

    [design] 어드민 로그아웃 시 좌단 nav바 스타일 변경

--- src/adminPage/shared/components/NavBarItem.jsx
+++ src/adminPage/shared/components/NavBarItem.jsx
@@ -1,6 +1,6 @@
 import {NavLink} from "react-router-dom";
 
-function NavBarItem({to, onClick, children})
+function NavBarItem({to, onClick, disabled, children})
 {
 	const commonStyle = `w-full h-full flex justify-center items-center
 	hover:border-2 hover:border-white
@@ -9,11 +9,11 @@ function NavBarItem({to, onClick, children})
 	const commonTextStyle = `text-neutral-200 hover:text-white active:text-neutral-400 invalid:text-neutral-600`;
 
 	const navLink = <NavLink to={to} 
-		className={ ({isActive})=>`${commonStyle} ${isActive ? "text-blue-400" : commonTextStyle}` }>
+		className={ ({isActive})=>`${commonStyle} ${disabled ? "pointer-events-none text-neutral-600": ""} ${isActive ? "text-blue-400" : commonTextStyle}` }>
 		{children}
 	</NavLink>;
 
-	const button = <button onClick={onClick} className={`${commonStyle} ${commonTextStyle}`}>{children}</button>;
+	const button = <button onClick={onClick} disabled={disabled} className={`${commonStyle} ${commonTextStyle}`}>{children}</button>;
 
 	return <li className="w-full h-12 flex justify-center items-center">
 		{to ? navLink : button}
