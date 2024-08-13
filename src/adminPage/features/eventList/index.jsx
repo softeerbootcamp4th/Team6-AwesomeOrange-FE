f0d91b521d47488adf477a798ac15792f199bf1f
lybell-art <futireflugel@gmail.com>
2024-08-13 16:37:51 +0900

    [feat] 기대평 검색 섹션 기본 레이아웃(1)

--- src/adminPage/features/eventList/index.jsx
+++ src/adminPage/features/eventList/index.jsx
@@ -1,25 +1,58 @@
+import SearchBar from "./SearchBar.jsx";
+import Button from "@common/components/Button.jsx";
+
+import Checkbox from "@common/components/Checkbox.jsx";
+
 
 
 function EventList()
 {
-	return <div>
-		<div>
-			<button>+ 이벤트 등록</button>
+	
+	return <div className="w-full h-full flex flex-col gap-4">
+		<div className="flex justify-end">
+			<Button>+ 이벤트 등록</Button>
 		</div>
-		<form>
-			<input type="text" />
-			<div>
-				<fieldset>
-					<legend>상태</legend>
-					<div>
-						<label>
-							<input type="checkbox" />
-							예정
-						</label>
-					</div>
-				</fieldset>
-			</div>
-		</form>
+		<SearchBar />
+		<div className="grid grid-cols-2">
+			<fieldset>
+				<legend>상태</legend>
+				<div>
+					<label>
+						<Checkbox name="event-state-scheduled" defaultChecked />
+						예정
+					</label>
+					<label>
+						<Checkbox name="event-state-ongoing" defaultChecked />
+						진행
+					</label>
+					<label>
+						<Checkbox name="event-state-pending-draw" defaultChecked />
+						추첨대기
+					</label>
+					<label>
+						<Checkbox name="event-state-ended" defaultChecked />
+						종료
+					</label>
+					<label>
+						<Checkbox name="event-state-temp" defaultChecked />
+						임시저장
+					</label>
+				</div>
+			</fieldset>
+			<fieldset>
+				<legend>레전</legend>
+				<div>
+					<label>
+						<Checkbox name="event-type-fcfs" defaultChecked />
+						선착순
+					</label>
+					<label>
+						<Checkbox name="event-type-draw" defaultChecked />
+						추첨
+					</label>
+				</div>
+			</fieldset>
+		</div>
 	</div>
 }
 
--- src/adminPage/shared/components/Container.jsx
+++ src/adminPage/shared/components/Container.jsx
@@ -4,7 +4,7 @@ function Container({ children }) {
   return (
     <div className="w-full min-h-screen flex">
       <NavBar />
-      <main className="w-full h-full min-h-screen flex-grow flex justify-center items-center">
+      <main className="w-full h-full min-h-screen p-4 flex-grow flex justify-center items-center">
         {children}
       </main>
     </div>
