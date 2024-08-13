8da06a219bd99fb39f966005e5aa42f1eb69bdc6
lybell-art <futireflugel@gmail.com>
2024-08-13 14:50:48 +0900

    [feat] 로그인 서버측과 연동

--- src/adminPage/shared/auth/LoginSection.jsx
+++ src/adminPage/shared/auth/LoginSection.jsx
@@ -1,17 +1,40 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { login } from "./store.js";
+import { fetchServer, handleError } from "@common/dataFetch/fetchServer.js";
 import Input from "@common/components/Input.jsx";
 import Button from "@common/components/Button.jsx";
 
+const loginErrorHandler = {
+	400: "잘못된 입력입니다!",
+	401: "로그인에 실패했습니다!"
+}
+
 function LoginSection()
 {
+	const navigate = useNavigate();
 	const [id, setId] = useState("");
 	const [password, setPassword] = useState("");
-	function onSubmit(e)
+	const [errorMessage, setErrorMessage] = useState("");
+	async function onSubmit(e)
 	{
 		e.preventDefault();
+		const config = {method: "post", body:{userName: id, password}};
+		setErrorMessage("");
+		fetchServer("/api/v1/admin/auth/signin", config)
+			.then( ({token})=>{
+				login(token);
+				navigate("/events", {replace: true});
+			} )
+			.catch(handleError(loginErrorHandler))
+			.catch( e=>{
+				setId("");
+				setPassword("");
+				setErrorMessage(e.message);
+			} );
 	}
-	return <form className="w-full h-full max-w-[32rem] max-h-[20rem] p-8 flex flex-col gap-8 bg-white shadow-md rounded-3xl group" onSubmit={onSubmit}>
+	return <form className="w-full h-full max-w-[32rem] max-h-[40rem] p-8 flex flex-col gap-8 bg-white shadow-md rounded-3xl group" onSubmit={onSubmit}>
 		<div className="flex flex-col gap-4">
 			<label>
 				<span className="text-detail-l text-neutral-600">아이디</span>
@@ -24,7 +47,10 @@ function LoginSection()
 				/>
 			</label>
 		</div>
-		<Button type="submit">로그인</Button>
+		<div className="flex flex-col relative items-center">
+			<Button className="w-full" type="submit">로그인</Button>
+			<span className="absolute -bottom-4 text-detail-l text-red-400">{errorMessage}</span>
+		</div>
 	</form>
 }
 
