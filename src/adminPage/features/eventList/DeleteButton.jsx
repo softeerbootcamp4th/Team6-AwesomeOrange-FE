59c14ba7c48c2f147530e408e145573c76eb06ab
lybell-art <futireflugel@gmail.com>
2024-08-14 18:08:13 +0900

    [feat] 삭제 버튼 클릭 시 모달 띄우는 동작 추가

--- src/adminPage/features/eventList/DeleteButton.jsx
+++ src/adminPage/features/eventList/DeleteButton.jsx
@@ -1,6 +1,8 @@
 import { fetchServer } from "@common/dataFetch/fetchServer.js";
 import { useMutation } from "@common/dataFetch/getQuery.js";
+import ConfirmModal from "@admin/modals/ConfirmModal.jsx";
 import Button from "@common/components/Button.jsx";
+import openModal from "@common/modal/openModal.js";
 
 function DeleteButton({selected, reset})
 {
@@ -17,11 +19,16 @@ function DeleteButton({selected, reset})
 				reset();
 			}
 		}
-	)
+	);
+	const deleteConfirmModal = <ConfirmModal title="삭제" description={<>
+		<p>이 동작은 다시 돌이킬 수 없습니다.</p>
+		<br/>
+		<p>{selected.keys().next().value}{selected.size > 1 && ` 외 ${selected.size - 1} 개의`} 이벤트를 삭제하시겠습니까?</p>
+	</>} onConfirm={mutate} />;
 
 	function onClick()
 	{
-		mutate();
+		openModal(deleteConfirmModal);
 	}
 	return <Button onClick={onClick} disabled={ selected.size === 0 }>삭제</Button>
 }
