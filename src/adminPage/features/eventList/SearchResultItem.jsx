f90f3008b637b484449a89af652dbc1e696ad7db
lybell-art <futireflugel@gmail.com>
2024-08-14 14:35:02 +0900

    [design] 체크박스와 버튼 추가

--- src/adminPage/features/eventList/SearchResultItem.jsx
+++ src/adminPage/features/eventList/SearchResultItem.jsx
@@ -1,11 +1,15 @@
 import tableTemplateCol from "./tableStyle.js";
 import EventStatus from "@admin/serverTime/EventStatus.js";
+import Button from "@common/components/Button.jsx";
+import Checkbox from "@common/components/Checkbox.jsx";
 import { formatDate } from "@common/utils.js";
 
 function SearchResultItem({eventId, name, startTime, endTime, eventType })
 {
 	return <div className={`${tableTemplateCol} h-8 text-body-s bg-white hover:bg-blue-100`}>
-		<div className="flex justify-center items-center">체크박스</div>
+		<div className="flex justify-center items-center">
+			<Checkbox />
+		</div>
 		<div className="flex justify-center items-center">{eventId}</div>
 		<div className="flex justify-center items-center overflow-hidden">
 			<span className="w-full text-ellipsis overflow-hidden whitespace-nowrap">{name}</span>
@@ -14,7 +18,9 @@ function SearchResultItem({eventId, name, startTime, endTime, eventType })
 		<div className="flex justify-center items-center">{formatDate(endTime, "YYYY-MM-DD hh:mm")}</div>
 		<div className="flex justify-center items-center">{eventType === "fcfs" ? "선착순" : eventType === "draw" ? "추첨" : "???"}</div>
 		<div className="flex justify-center items-center"><EventStatus startTime={startTime} endTime={endTime} /></div>
-		<button>상세보기</button>
+		<div className="flex justify-center items-center">
+			<Button styleType="ghost" className="px-2 py-1 text-body-s">상세보기</Button>
+		</div>
 	</div>
 }
 
