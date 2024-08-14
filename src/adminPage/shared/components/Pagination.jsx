e8e8136f2a3d74660b908eace3a0f5b2eea2e928
lybell-art <futireflugel@gmail.com>
2024-08-14 17:36:20 +0900

    [design] 페이지네이션 컴포넌트 디자인

--- src/adminPage/shared/components/Pagination.jsx
+++ src/adminPage/shared/components/Pagination.jsx
@@ -10,18 +10,26 @@ function getPaginationItem(currentPage, maxPage, length)
 	return Array.from({length}, (_, i)=>currentPage - prevDelta + i);
 }
 
+function PaginationButton({onClick, disabled, highlighted, children})
+{
+	return <button onClick={onClick} disabled={disabled} type="button"
+	className={`w-8 text-body-l select-none ${highlighted ? "text-blue-400 font-bold" : "text-neutral-700 font-medium hover:text-black"} disabled:text-neutral-300`}>
+		{children}
+	</button>
+}
+
 function Pagination({currentPage, setPage: _setPage, maxPage, length=5})
 {
 	const setPage = (index)=>()=>_setPage(clamp(index, 1, maxPage));
 
-	return <div>
-		<button onClick={setPage(currentPage - 5)} type="button"> 뒤로 5칸더가기 </button>
-		<button onClick={setPage(currentPage - 1)} type="button"> 뒤로 1칸더가기 </button>
+	return <div className="flex gap-4">
+		<PaginationButton onClick={setPage(currentPage - 5)} disabled={currentPage - 2 < 1}>&lt;&lt;</PaginationButton>
+		<PaginationButton onClick={setPage(currentPage - 1)} disabled={currentPage - 1 < 1}>&lt;</PaginationButton>
 		{
-			getPaginationItem(currentPage, maxPage, length).map( (i)=><button key={i} onClick={setPage(i)} type="button">{i}</button> )
+			getPaginationItem(currentPage, maxPage, length).map( (i)=><PaginationButton key={i} onClick={setPage(i)} highlighted={i === currentPage}>{i}</PaginationButton> )
 		}
-		<button onClick={setPage(currentPage + 1)} type="button"> 앞으로 1칸더가기 </button>
-		<button onClick={setPage(currentPage + 5)} type="button"> 앞으로 5칸더가기 </button>
+		<PaginationButton onClick={setPage(currentPage + 1)} disabled={currentPage + 1 > maxPage}>&gt;</PaginationButton>
+		<PaginationButton onClick={setPage(currentPage + 5)} disabled={currentPage + 2 > maxPage}>&gt;&gt;</PaginationButton>
 	</div>
 }
 
