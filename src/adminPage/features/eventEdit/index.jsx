a06116e3497f0a0aa3ed70614eacc2d146267553
lybell <futireflugel@gmail.com>
2024-08-16 00:17:45 +0900

    [feat] 이벤트 종류 선택 탭바 추가

--- src/adminPage/features/eventEdit/index.jsx
+++ src/adminPage/features/eventEdit/index.jsx
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import { eventEditReducer, setDefaultState } from "./businessLogic/reducer.js";
 
 import EventBaseDataInput from "./EventBaseDataInput.jsx";
+import EventDetailInput from "./EventDetailInput.jsx";
 import Button from "@common/components/Button.jsx";
 
 function EventEditor({mode, initialData = null} = {}) {
@@ -27,6 +28,12 @@ function EventEditor({mode, initialData = null} = {}) {
     </div>
     <div className="w-full flex flex-col gap-3">
       <EventBaseDataInput state={state} dispatch={dispatch} mode={mode} />
+      <div className="grid grid-cols-[6rem_1fr] items-start gap-2">
+        <span className="py-2 text-center">
+          이벤트 종류<sup className="text-red-500">*</sup>
+        </span>
+        <EventDetailInput state={state} dispatch={dispatch} mode={mode} />
+      </div>
     </div>
   </form>
 }
