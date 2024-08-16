d1d1cb031a4de5eef977409343f07db022e320e6
lybell-art <futireflugel@gmail.com>
2024-08-16 12:27:25 +0900

    [feat] 어드민 이벤트등록 선착순 상단부분 아주약간만 레이아웃 추가

--- src/adminPage/features/eventEdit/FcfsInput.jsx
+++ src/adminPage/features/eventEdit/FcfsInput.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { Input } from "@admin/components/SmallInput.jsx";
+import Checkbox from "@common/components/Checkbox.jsx";
+
 function FcfsInput() {
-  return <>선착순 이벤트임</>;
+  const [isOpenTimeBatch, SetOpenTimeBatch] = useState(false);
+  const [isCloseTimeBatch, SetCloseTimeBatch] = useState(false);
+
+  return <div>
+    <div>
+      <label>
+        <Checkbox checked={isOpenTimeBatch} onChange={SetOpenTimeBatch}/>
+        오픈시간 일괄 설정
+        <Input type="time" disabled={!isOpenTimeBatch}/>
+      </label>
+      <label>
+        <Checkbox checked={isCloseTimeBatch} onChange={SetCloseTimeBatch}/>
+        종료 시간 일괄 설정
+        <Input type="time" disabled={!isCloseTimeBatch}/>
+      </label>
+      <button>자동 채우기</button>
+    </div>
+    <div>
+      {}
+    </div>
+  </div>;
 }
 
 export default FcfsInput;
