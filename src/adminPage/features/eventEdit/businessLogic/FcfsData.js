4176d03c25670b7165d49c4424e9af485006c0c3
lybell-art <futireflugel@gmail.com>
2024-08-17 10:10:24 +0900

    [fix] 선착순 각 아이템 시간 조작 시 부적절한 값이 들어갈 수 있는 버그 수정

--- src/adminPage/features/eventEdit/businessLogic/FcfsData.js
+++ src/adminPage/features/eventEdit/businessLogic/FcfsData.js
@@ -1,11 +1,111 @@
 import makeUUID from "./makeUUID.js";
-import { DEFAULT_END_TIME, DEFATLT_PARTICIPANT } from "./constants.js";
+import { DEFAULT_END_TIME, DEFATLT_PARTICIPANT, STEP } from "./constants.js";
 
 const MINUTES = 60 * 1000;
 const ONE_DAY = 24 * 60 * MINUTES;
 
 function extractHourMinutes(date) {
   return date.getHours() * 60 + date.getMinutes();
+}
+
+function extractDate(date) {
+  return new Date( date.getFullYear(), date.getMonth(), date.getDate() );
+}
+
+function getDateTime(date, minutes)
+{
+  return date.valueOf() + minutes * MINUTES;
+}
+
+function verityTime(item, {startTime, endTime, prevSnapshot={}}={})
+{
+  const {start, end, date, ...rest} = item;
+
+  let [newStart, newEnd] = [start ?? 0, end ?? DEFAULT_END_TIME];
+
+  // end가 start보다 더 작으면, 반드시 start가 end보다 더 작도록 치환
+  if(newStart >= newEnd)
+  {
+    if(prevSnapshot.start !== newStart)
+    {
+      if(newEnd === 0) {
+        newStart = 0;
+        newEnd = newEnd + STEP;
+      }
+      else newStart = newEnd - STEP;
+    }
+    else 
+    {
+      if(newStart === DEFAULT_END_TIME) {
+        newStart = DEFAULT_END_TIME - STEP;
+        newEnd  = DEFAULT_END_TIME
+      }
+      else newEnd = newStart + STEP;
+    } 
+  }
+
+  // date를 추가한 타임스탬프를 계산
+  const startValue = date.valueOf() + newStart * MINUTES;
+  const endValue = date.valueOf() + newEnd * MINUTES;
+
+  // 만약 종료 시간이 전체 시작 시간보다 더 빠르거나, 시작 시간이 전체 종료 시간보다 더 늦는다면 전체를 무효화시킴.(제거)
+  if(endTime <= startValue || startTime >= endValue) return null;
+
+  // 시작시간과 종료시간을 전체 시작 시간에 맞도록 클리핑함.
+  // 클리핑한 대상의 새로운 시작 시간이 23:55이거나 새로운 종료 시간이 00:00이면 무효한 시간으로 판별.(제거)
+  if(startValue < startTime) 
+  {
+    newStart = extractHourMinutes(startTime);
+    if(newStart === DEFAULT_END_TIME) return null;
+  }
+  if(endValue > endTime)
+  {
+    newEnd = extractHourMinutes(endTime);
+    if(newEnd === 0) return null;
+  }
+
+  return {
+    date,
+    start: newStart,
+    end: newEnd,
+    ...rest
+  };
+}
+
+function verifyItem(item, {startTime, endTime, prevSnapshot={}}={})
+{
+  const {date, ...rest} = item;
+
+  // 만약 date가 입력 중이라면 item 그대로 반환. date가 빈 값이어도 item 그대로 반환.
+  if(!date) return item;
+
+  // date를 수정했을 때, date가 시작점/끝점에 맞도록 date를 조정함.
+  let newDate = date;
+  const startDate = extractDate(startTime);
+  const endDate = extractDate(endTime);
+
+  if(date < startDate || date > endDate)
+  {
+    if(prevSnapshot.date !== null) newDate = prevSnapshot.date;
+    else if(date < startDate) newDate = startDate;
+    else newDate = endDate;
+  }
+
+  const verified = verityTime({date: newDate, ...rest}, {startTime, endTime, prevSnapshot});
+  if(verified === null && prevSnapshot.date === null) return prevSnapshot;
+  return verified;
+}
+
+function verifyItems(map, {startTime, endTime, prevSnapshot = new Map()})
+{
+  const result = new Map(map);
+  for(let [key, unstableItem] of map)
+  {
+    const verifiedValue = verifyItem(unstableItem, {startTime, endTime, prevSnapshot: prevSnapshot.get(key)});
+    if(verifiedValue === null) result.delete(key);
+    else result.set(key, verifiedValue);
+  }
+  return result;
 }
 
 function getDefaultFcfsArray(
@@ -22,30 +122,24 @@ function getDefaultFcfsArray(
   const endDate = Math.floor((endTime.valueOf() - TIME_ZONE_OFFSET) / ONE_DAY);
 
   const length = endDate - startDate + 1;
-  const trueStartDate = startDate * ONE_DAY + TIME_ZONE_OFFSET;
+  const baseDate = startDate * ONE_DAY + TIME_ZONE_OFFSET;
+  const result = [];
 
-  return Array.from({ length }, (_, i) => {
-    const dateValue = trueStartDate + i * ONE_DAY;
-    const start =
-      dateValue + config.start * MINUTES < startTime
-        ? extractHourMinutes(startTime)
-        : config.start;
-    const end =
-      dateValue + config.end * MINUTES > endTime
-        ? extractHourMinutes(endTime)
-        : config.end;
+  for(let i=0; i<length; i++)
+  {
+    const rawItem = {
+      date: new Date(baseDate + i * ONE_DAY),
+      start: config.start ?? 0,
+      end: config.end ?? DEFAULT_END_TIME,
+      participantCount: config.participantCount ?? DEFATLT_PARTICIPANT,
+      prizeInfo: "",
+    }
 
-    return [
-      `auto_made_${i}`,
-      {
-        date: new Date(dateValue),
-        start,
-        end,
-        participantCount: config.participantCount,
-        prizeInfo: "",
-      },
-    ];
-  });
+    const item = verifyItem(rawItem, {startTime, endTime});
+    if(item !== null) result.push([`auto_made_${i}`, item]);
+  }
+
+  return result;
 }
 
 function convertServerDataToClient({ id, startTime, endTime, ...rest }) {
@@ -91,8 +185,9 @@ class FcfsData {
   }
   add(
     data = {
-      startTime: null,
-      endTime: null,
+      date: null,
+      start: 0,
+      end: DEFAULT_END_TIME,
       participantCount: DEFATLT_PARTICIPANT,
       prizeInfo: "",
     },
@@ -106,56 +201,27 @@ class FcfsData {
     newData.map.delete(key);
     return newData;
   }
-  modify(key, data = {}) {
+  modify(key, data, { startTime, endTime } ) {
     const newData = new FcfsData(this.map);
     const oldItem = newData.map.get(key);
-    newData.map.set(key, { ...oldItem, ...data });
+    const verified = verifyItem({...oldItem, ...data}, {startTime, endTime, prevSnapshot: oldItem});
+    if(verified === null) newData.map.delete(key);
+    else newData.map.set(key, verified);
     return newData;
   }
-  modifyAll(data, { startTime, endTime } = {}) {
+  modifyAll(data, { startTime, endTime } ) {
     const newData = new FcfsData(this.map);
-    const { start: newStart, end: newEnd, ...others } = data;
-    delete others.date;
-
-    for (let [key, item] of this.map) {
-      const itemStartTime = item.date.valueOf() + newStart * MINUTES;
-      const itemEndTime = item.date.valueOf() + newEnd * MINUTES;
-
-      const start =
-        newStart === undefined
-          ? item.start
-          : itemStartTime < startTime
-            ? extractHourMinutes(startTime)
-            : newStart;
-      const end =
-        newEnd === undefined
-          ? item.end
-          : itemEndTime > endTime
-            ? extractHourMinutes(endTime)
-            : newEnd;
-
-      newData.map.set(key, { ...item, start, end, ...others });
+    for(let [key, value] of this.map)
+    {
+      newData.map.set(key, {...value, ...data});
     }
+    newData.map = verifyItems(newData.map, {startTime, endTime, prevSnapshot: this.map});
     return newData;
   }
   verifyDate(startTime, endTime) {
-    const newData = new FcfsData(this.map);
-    for (let [key, item] of this.map) {
-      const itemStartTime = item.date.valueOf() + item.start * MINUTES;
-      const itemEndTime = item.date.valueOf() + item.end * MINUTES;
+    const verifiedMap = verifyItems(this.map, {startTime, endTime, prevSnapshot: this.map});
 
-      if (itemStartTime > endTime || itemEndTime < startTime) {
-        newData.map.delete(key);
-        continue;
-      }
-      if (startTime <= itemStartTime && itemEndTime <= endTime) continue;
-
-      const copied = { ...item };
-      if (itemEndTime > endTime) copied.end = extractHourMinutes(endTime);
-      if (itemStartTime < startTime)
-        copied.start = extractHourMinutes(startTime);
-      newData.map.set(key, copied);
-    }
+    const newData = new FcfsData(verifiedMap);
     return newData;
   }
   *[Symbol.iterator]() {
--- src/adminPage/features/eventEdit/businessLogic/constants.js
+++ src/adminPage/features/eventEdit/businessLogic/constants.js
@@ -1,3 +1,4 @@
 export const DEFAULT_END_TIME = 1435;
 export const DEFATLT_PARTICIPANT = 100;
 export const MAX_GRADE = 10;
+export const STEP = 5;
