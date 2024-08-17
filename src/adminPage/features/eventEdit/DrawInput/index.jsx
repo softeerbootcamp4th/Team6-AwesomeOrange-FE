696855d543d5ea5788f0e6be6fa62104b118764f
lybell-art <futireflugel@gmail.com>
2024-08-17 19:45:42 +0900

    [design] 기본 폰트 regular로 변경

--- src/adminPage/features/eventEdit/DrawInput/index.jsx
+++ src/adminPage/features/eventEdit/DrawInput/index.jsx
@@ -2,12 +2,12 @@ import DrawSectionTitle from "./DrawSectionTitle.jsx";
 import DrawMetadataInput from "./DrawMetadataInput.jsx";
 
 function DrawInput() {
-  return <div className="w-full flex flex-col items-center gap-8">
-    <div className="flex flex-col gap-4 w-full max-w-[800px]">
+  return <div className="w-full flex flex-col items-center gap-10">
+    <div className="flex flex-col gap-3 w-full max-w-[800px]">
       <DrawSectionTitle>인원수 및 경품 설정</DrawSectionTitle>
       <DrawMetadataInput />
     </div>
-    <div className="flex flex-col gap-4 w-full max-w-[800px]">
+    <div className="flex flex-col gap-3 w-full max-w-[800px]">
       <DrawSectionTitle>정책 설정</DrawSectionTitle>
       <div>
         <div className="grid grid-cols-[3fr_1fr] font-bold">
