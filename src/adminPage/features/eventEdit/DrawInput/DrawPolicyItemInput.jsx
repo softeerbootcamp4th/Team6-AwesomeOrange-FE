d5a5d4dae8ffa4d32117da8be4018b0efa57fd8d
lybell-art <futireflugel@gmail.com>
2024-08-17 22:07:31 +0900

    [feat] 이벤트 등록-정책 작성 폼 추가

---
+++ src/adminPage/features/eventEdit/DrawInput/DrawPolicyItemInput.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import {EventEditDispatchContext} from "../businessLogic/context.js";
+import { Input } from "@admin/components/SmallInput.jsx";
+
+const POLICY_ENUM = [
+	["WriteComment", "기대평 작성"],
+    ["ParticipateEvent", "이벤트 참여"]
+];
+
+function DrawPolicyItemInput({action, score, uniqueKey})
+{
+	const dispatch = useContext(EventEditDispatchContext);
+	const modify = (value)=>dispatch({type: "modify_draw_policy", key: uniqueKey, value});
+
+	return <>
+		<select
+			className="invalid:text-neutral-400"
+			value={action} 
+			required
+			onChange={(e)=>modify({action: e.target.value})}
+		>
+			<option value="">-정책을 설정하세요-</option>
+			{
+				POLICY_ENUM.map( ([key, text])=><option value={key} key={key}>{text}</option> )
+			}
+		</select>
+		<Input text={score} required
+			setText={ (value)=>modify({score: (Number.isNaN(+value) ? 0 : +value)}) } 
+			inputMode="numeric" pattern="[0-9]+" size="6" placeholder="점수 배율"
+		/>
+		<div>X</div>
+	</>
+}
+
+export default DrawPolicyItemInput;
--- src/adminPage/features/eventEdit/DrawInput/index.jsx
+++ src/adminPage/features/eventEdit/DrawInput/index.jsx
@@ -1,5 +1,6 @@
 import DrawSectionTitle from "./DrawSectionTitle.jsx";
 import DrawMetadataInput from "./DrawMetadataInput.jsx";
+import DrawPolicyInput from "./DrawPolicyInput.jsx";
 
 function DrawInput() {
   return <div className="w-full flex flex-col items-center gap-10">
@@ -7,19 +8,9 @@ function DrawInput() {
       <DrawSectionTitle>인원수 및 경품 설정</DrawSectionTitle>
       <DrawMetadataInput />
     </div>
-    <div className="flex flex-col gap-3 w-full max-w-[800px]">
+    <div className="flex flex-col gap-6 w-full max-w-[800px]">
       <DrawSectionTitle>정책 설정</DrawSectionTitle>
-      <div>
-        <div className="grid grid-cols-[3fr_1fr] font-bold">
-          <div className="text-center">액션</div>
-          <div className="text-center">배율</div>
-        </div>
-        <div className="grid grid-cols-[3fr_1fr] font-bold">
-          <div className="text-center">1등</div>
-          <div className="text-center">명</div>
-        </div>
-        <button>추가하기</button>
-      </div>
+      <DrawPolicyInput />
     </div>
   </div>
 }
