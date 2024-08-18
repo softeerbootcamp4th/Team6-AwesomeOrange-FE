e3934e5e4815c6c8272bf7caff948e2f9bc189f6
lybell-art <futireflugel@gmail.com>
2024-08-18 23:06:48 +0900

    [fix] 페이지네이션의 최대 페이지가 설정된 페이지 미만일 시 최대 페이지만큼만 띄우도록 변경

--- src/adminPage/shared/components/Pagination.jsx
+++ src/adminPage/shared/components/Pagination.jsx
@@ -4,6 +4,7 @@ function getPaginationItem(currentPage, maxPage, length) {
   let prevDelta = length % 2 === 1 ? (length - 1) / 2 : length / 2 - 1;
   let postDelta = length % 2 === 1 ? (length - 1) / 2 : length / 2;
 
+  if (maxPage < length) return Array.from({ length: maxPage }, (_, i) => i + 1);
   if (currentPage - prevDelta <= 0) return Array.from({ length }, (_, i) => i + 1);
   if (currentPage + postDelta > maxPage)
     return Array.from({ length }, (_, i) => maxPage - length + i + 1);
