7b4f2ebe017dac7914f49f9b22b57b0d25606221
lybell-art <futireflugel@gmail.com>
2024-08-18 12:19:59 +0900

    [feat] 이벤트 상세 선착순 아이템 렌더링 추가

---
+++ src/adminPage/features/eventDetail/fcfs/EventFcfsDataItem.jsx
@@ -0,0 +1,14 @@
+import { formatDate } from "@common/utils.js";
+
+function EventFcfsDataItem({id, startTime, endTime, participantCount, prizeInfo})
+{
+	return <>
+		<p className="text-center font-bold">{id}</p>
+		<p className="text-center">{formatDate(startTime, "M/DD")}</p>
+		<p className="text-center">{formatDate(startTime, "hh:mm")} ~ {formatDate(endTime, "hh:mm")}</p>
+		<p className="text-center">{participantCount}명</p>
+		<p>{prizeInfo}</p>
+	</>
+}
+
+export default EventFcfsDataItem;
--- src/adminPage/features/eventDetail/fcfs/index.jsx
+++ src/adminPage/features/eventDetail/fcfs/index.jsx
@@ -1,19 +1,21 @@
-function EventFcfsDataItem({data})
-{
-	return <><p>준비중</p><p>준비중</p><p>준비중</p><p>준비중</p></>
-}
+import EventFcfsDataItem from "./EventFcfsDataItem.jsx";
 
 function EventFcfsDataRenderer({data})
 {
-	const gridStyle = "grid grid-cols-[3rem_1.5fr_4rem_1fr] gap-4 justify-center items-center text-body-m";
-	return <section>
-		<div className={`${gridStyle} h-10 font-bold`}>
-			<div className="text-center">날짜</div>
-			<div className="text-center">이벤트 시간</div>
-			<div className="text-center">당첨자 수</div>
-			<div className="text-center">경품</div>
+	const gridStyle = "grid grid-cols-[1fr_3rem_1fr_5rem_3fr] gap-4 justify-center items-center text-body-m";
+	const headerStyle = `${gridStyle} min-h-12 py-2 bg-neutral-50 rounded-lg text-black font-bold text-center
+	*:relative *:w-full *:after:h-full *:after:absolute *:after:-right-2 *:after:border-r *:after:border-neutral-200
+	`;
+
+	return <section className="flex flex-col gap-2">
+		<div className={headerStyle}>
+			<div>ID</div>
+			<div>날짜</div>
+			<div>이벤트 시간</div>
+			<div>당첨자 수</div>
+			<div className="last:after:border-r-0">경품</div>
 		</div>
-		<div className={`${gridStyle} h-8 font-medium`}>
+		<div className={`${gridStyle} h-8 font-regular`}>
 			{data.map( (item)=><EventFcfsDataItem key={item.id} {...item} /> )}
 		</div>
 	</section>
