c34a53a91f393d09fd73f695a0a72abfa5302c88
darkdulgi <daniel99285@gmail.com>
2024-08-20 18:40:13 +0900

    [feat] 어드민 유저 조회 컴포넌트 대충 작성

---
+++ src/adminPage/features/users/Users.jsx
@@ -0,0 +1,49 @@
+// import { useQuery } from "@common/dataFetch/getQuery.js";
+// import { fetchServer } from "@common/dataFetch/fetchServer.js";
+// import Pagination from "@admin/components/Pagination";
+// import { useState } from "react";
+
+export default function Comments({
+  searchString,
+}) {
+  searchString;
+  // const [page, setPage] = useState(1);
+  // const data = useQuery(
+  //   "admin-users",
+  //   () =>
+  //     fetchServer(
+  //       `/api/v1/admin/호출!!!!!!`,
+  //     )
+  //       .then((res) => {
+  //         console.log(res);
+  //         return res;
+  //       })
+  //       .catch((e) => {
+  //         console.log(e);
+  //       }),
+  //   [page, searchString],
+  // );
+
+  return (
+    <div className="mt-1 mb-5 flex flex-col items-center gap-1 w-full">
+      {/* {data.comments.map((comment) => (
+        <div
+          key={comment.id}
+          className="w-full py-1 grid grid-cols-[1fr_5fr_15fr] bg-neutral-50 items-center hover:bg-blue-100"
+        >
+          <input
+            type="checkbox"
+            className="w-4 h-4 place-self-center"
+          />
+
+          <div className="place-self-center flex items-center gap-1 text-body-s">
+          </div>
+
+          <span className="pr-4 overflow-hidden text-body-s text-ellipsis">{comment.content}</span>
+        </div>
+      ))}
+
+      <Pagination currentPage={page} setPage={setPage} maxPage={data.totalPages} /> */}
+    </div>
+  );
+}
--- src/adminPage/features/users/index.jsx
+++ src/adminPage/features/users/index.jsx
@@ -1,3 +1,49 @@
-export default function Users() {
-  return <>유저들</>;
+import Suspense from "@common/components/Suspense";
+import Loading from "./Loading.jsx";
+import Users from "./Users.jsx";
+import { useState } from "react";
+
+export default function AdminCommentID() {
+  const [formString, setFormString] = useState("");
+  const [searchString, setSearchString] = useState("");
+
+  function searchComment(e) {
+    e.preventDefault();
+    setSearchString(formString);
+  }
+
+  return (
+    <div className="flex flex-col w-full items-center">
+      <form onSubmit={searchComment} className="mt-3 w-full relative">
+        <input
+          type="text"
+          value={formString}
+          onChange={(e) => setFormString(e.target.value)}
+          placeholder="검색 단어 입력"
+          className="bg-neutral-50 focus:bg-white w-full px-4 py-2 rounded-lg text-body-s"
+        />
+
+        <img
+          onClick={searchComment}
+          src="/icons/search.png"
+          alt="검색"
+          className="cursor-pointer absolute top-1/2 -translate-y-1/2 right-4"
+        />
+      </form>
+
+      <div className="mt-3 py-1 w-full grid grid-cols-[1fr_5fr_15fr] bg-blue-50 place-items-center text-body-s select-none">
+        <span className="cursor-pointer">
+          선택
+        </span>
+        <span>작성 시간</span>
+        <span>기대평 내용</span>
+      </div>
+
+      <Suspense fallback={<Loading />}>
+        <Users
+          searchString={searchString}
+        />
+      </Suspense>
+    </div>
+  );
 }
