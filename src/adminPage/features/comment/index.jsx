084913e5a8fe75d8c8280162f6a0d014f2583205
darkdulgi <daniel99285@gmail.com>
2024-08-20 12:27:55 +0900

    [feat] 이벤트 검색 방향키로 선택 가능하게 함

--- src/adminPage/features/comment/index.jsx
+++ src/adminPage/features/comment/index.jsx
@@ -7,12 +7,12 @@ export default function AdminComment() {
   const [formString, setFormString] = useState("");
   const [isSpread, setIsSpread] = useState(false);
   const [searchList, setSearchList] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(-1);
 
-  function autoCorrect() {
-    fetchServer(`/api/v1/admin/events/hints?search=${formString}`)
+  function autoCorrect(str) {
+    fetchServer(`/api/v1/admin/events/hints?search=${str}`)
       .then((res) => {
         setSearchList(res);
-        setIsSpread(true);
       })
       .catch((e) => {
         console.log(e);
@@ -20,30 +20,53 @@ export default function AdminComment() {
   }
 
   function onChangeForm(e) {
-    const filteredString = e.target.value.replace(/[^0-9]/g, "");
+    let newString = e.target.value.replace(/[^0-9]/g, "");
 
-    if (!filteredString) {
-      setFormString("");
-    } else if (filteredString.length <= 6) {
-      setFormString("HD_" + filteredString);
-    } else if (filteredString.length <= 9) {
-      setFormString("HD_" + filteredString.slice(0, 6) + "_" + filteredString.slice(6));
+    if (!newString) {
+      newString = "";
+    } else if (newString.length <= 6) {
+      newString = "HD_" + newString;
+    } else if (newString.length <= 9) {
+      newString = "HD_" + newString.slice(0, 6) + "_" + newString.slice(6);
     } else return;
 
-    if (filteredString.length >= 6) {
-      autoCorrect();
-    } else setIsSpread(false);
+    if (newString !== formString) {
+      if (newString.length >= 9) {
+        setSelectedEvent(-1);
+        setIsSpread(true);
+        autoCorrect(newString);
+      } else {
+        setIsSpread(false);
+      }
+    }
+    setFormString(newString);
   }
 
   function searchEvent(e, eventId) {
     e.preventDefault();
 
     const eventIDRegex = /^HD_\d{6}_\d{3}$/;
-    const searchID = eventId ? eventId : formString;
+    const searchID = eventId ?? formString;
 
     if (eventIDRegex.test(searchID)) {
       navigate(`/comments/${searchID}`);
     }
+  }
+
+  function onKeyDown(e) {
+    if (!isSpread || (e.key !== "ArrowUp" && e.key !== "ArrowDown")) return;
+    e.preventDefault();
+    let nextIndex = selectedEvent;
+
+    if (e.key === "ArrowUp") {
+      if (nextIndex === -1) nextIndex++;
+      nextIndex = (nextIndex - 1 + searchList.length) % searchList.length;
+    } else {
+      nextIndex = (nextIndex + 1) % searchList.length;
+    }
+
+    setSelectedEvent(nextIndex);
+    setFormString(searchList[nextIndex].eventId);
   }
 
   return (
@@ -53,18 +76,20 @@ export default function AdminComment() {
         inputMode="numeric"
         onChange={onChangeForm}
         value={formString}
+        onKeyDown={onKeyDown}
         placeholder="ID (숫자 9자리)"
         className={`z-10 bg-neutral-50 focus:bg-white px-4 py-2 w-full ${isSpread ? "rounded-t-md" : "rounded-md"}`}
       />
 
       <div
-        className={`absolute max-h-40 top-full border overflow-y-auto w-full rounded-b-md px-3 py-2 flex flex-col gap-2 ${!isSpread && "hidden"}`}
+        className={`absolute top-full border overflow-y-auto w-full rounded-b-md px-3 py-2 flex flex-col gap-2 ${!isSpread && "hidden"}`}
       >
-        {searchList.map((evt) => (
+        {searchList.map((evt, index) => (
           <li
             key={evt.eventId}
+            onMouseEnter={() => setSelectedEvent(index)}
             onClick={(e) => searchEvent(e, evt.eventId)}
-            className={`cursor-pointer list-none w-full rounded px-1 flex hover:bg-blue-200`}
+            className={`cursor-pointer list-none w-full rounded px-1 flex ${index === selectedEvent && "bg-blue-200"}`}
           >
             <span className="w-40">{evt.eventId}</span>
             <span>{evt.name}</span>
