9b0c695fc3cb8f84d21338641325114976479c34
lybell-art <futireflugel@gmail.com>
2024-08-21 16:23:34 +0900

    [fix] 디바이스 ratio 초기상태 갱신 안 되는 거 수정

--- src/mainPage/features/interactions/distanceDriven/useDeviceRatio.js
+++ src/mainPage/features/interactions/distanceDriven/useDeviceRatio.js
@@ -4,6 +4,7 @@ import throttleRaf from "@common/throttleRaf.js";
 function useDeviceRatio() {
   const [ratio, setRatio] = useState(1);
   useEffect(() => {
+    setRatio(Math.hypot(window.innerWidth, window.innerHeight) / 2);
     const onResize = throttleRaf(() => {
       setRatio(Math.hypot(window.innerWidth, window.innerHeight) / 2);
     });
