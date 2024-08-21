d61565753a00923da39da7ae09fb916a64b7440b
lybell-art <futireflugel@gmail.com>
2024-08-21 15:21:33 +0900

    [modify] useMountDragEvent에 enabled 옵션 추가

--- src/mainPage/shared/hooks/useMountDragEvent.js
+++ src/mainPage/shared/hooks/useMountDragEvent.js
@@ -1,11 +1,13 @@
 import { useState, useRef, useEffect, useCallback } from "react";
 import throttleRaf from "@common/throttleRaf.js";
 
-function useMountDragEvent({ onDragStart: userDragStart, onDrag, onDragEnd: userDragEnd } = {}) {
+function useMountDragEvent({ onDragStart: userDragStart, onDrag, onDragEnd: userDragEnd, enabled = true } = {}) {
   const [dragState, setDragState] = useState(false);
   const isDragging = useRef(false);
 
   useEffect(() => {
+    if(!enabled) return;
+
     const onPointerMove = throttleRaf((e) => {
       if (e.pointerType === "touch") return;
       if (!isDragging.current) return;
@@ -17,6 +19,18 @@ function useMountDragEvent({ onDragStart: userDragStart, onDrag, onDragEnd: user
       if (!isDragging.current) return;
       onDrag({ x: clientX, y: clientY });
     });
+
+    window.addEventListener("pointermove", onPointerMove);
+    window.addEventListener("touchmove", onTouchMove);
+    return () => {
+      window.removeEventListener("pointermove", onPointerMove);
+      window.removeEventListener("touchmove", onTouchMove);
+    };
+  }, [onDrag, enabled]);
+
+  useEffect(() => {
+    if(!enabled) return;
+
     const onDragEnd = (e) => {
       if (!isDragging.current) return;
       isDragging.current = false;
@@ -24,29 +38,26 @@ function useMountDragEvent({ onDragStart: userDragStart, onDrag, onDragEnd: user
       userDragEnd?.(e);
     };
 
-    window.addEventListener("pointermove", onPointerMove);
     window.addEventListener("pointerup", onDragEnd);
     window.addEventListener("pointercancel", onDragEnd);
-    window.addEventListener("touchmove", onTouchMove);
     window.addEventListener("touchend", onDragEnd);
     window.addEventListener("touchcancel", onDragEnd);
     return () => {
-      window.removeEventListener("pointermove", onPointerMove);
       window.removeEventListener("pointerup", onDragEnd);
       window.removeEventListener("pointercancel", onDragEnd);
-      window.removeEventListener("touchmove", onTouchMove);
       window.removeEventListener("touchend", onDragEnd);
       window.removeEventListener("touchcancel", onDragEnd);
     };
-  }, [onDrag, userDragEnd]);
+  }, [userDragEnd, enabled]);
 
   const onPointerDown = useCallback(
     (e) => {
+      if(!enabled) return;
       isDragging.current = true;
       setDragState(true);
       userDragStart?.({ x: e.clientX, y: e.clientY });
     },
-    [userDragStart],
+    [userDragStart, enabled],
   );
 
   return {
