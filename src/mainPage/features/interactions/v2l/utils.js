ae8344f1c18ae7116cab20301ec3eba640bbf33a
lybell-art <futireflugel@gmail.com>
2024-08-22 14:45:35 +0900

    [feat] 퍼즐의 이어짐 상태를 판별하는 함수 추가

--- src/mainPage/features/interactions/v2l/utils.js
+++ src/mainPage/features/interactions/v2l/utils.js
@@ -104,17 +104,30 @@ export function checkPuzzle(pieces, answer) {
   return pieces.every((piece, i) => piece.isCorrect(answer[i]));
 }
 
-// export function getCurrentPuzzleState(pieces, width, height) {
-//   let traced = Array.from({ length: width }, () => new Array(height).fill(false));
-//   const stack = [];
-//   let cursor = 0;
-
-//   if(pieces[0].getConnectData & )
-
-//   while (true) {
-//     const x = cursor % width;
-//     const y = Math.floor(cursor / width);
-
-//     if(pieces[cursor]) 
-//   }
-// }
+export function getLinkedPuzzleState(pieces, width, height)
+{
+  const linked = [];
+  let [x, y] = [0, 0];
+  let prev = LEFT;
+  while(x >= 0 && x < width && y >= 0 && y < height)
+  {
+    // 직전 연결된 상태와 현재 커서가 연결되어 있지 않으면 return
+    const cursor = y * width + x;
+    const connectData = pieces[cursor].getConnectData();
+    if((prev & connectData) === 0) return linked;
+    // 연결되어 있다면 인덱스를 배열에 넣음
+    linked.push(cursor);
+    // 다음 연결된 상태를 가져옴
+    prev = connectData ^ prev;
+    // 다음 연결 상태를 기반으로 커서를 옮기고, 다음 연결 상태를 반전시킴 (이전 커서에서 오른쪽 = 다음 커서에서 왼쪽)
+    switch(prev)
+    {
+      case LEFT : x--; prev=RIGHT; break;
+      case RIGHT : x++; prev=LEFT; break;
+      case UP: y--; prev=DOWN; break;
+      case DOWN: y++; prev=UP; break;
+      default: return linked;
+    }
+  }
+  return linked;
+}
