ef64cfcb7de6a7009e72a35386b4b777e60c6408
lybell-art <futireflugel@gmail.com>
2024-08-23 17:12:05 +0900

    [feat] 숏 풀링을 이용하여 추첨 버튼 및 결과보기 추가

--- packages/adminPage/src/features/eventDetail/drawButton/DrawButton.jsx
+++ packages/adminPage/src/features/eventDetail/drawButton/DrawButton.jsx
@@ -1,9 +1,9 @@
+import { useState, useEffect, useRef } from "react"; 
 import { useParams } from "react-router-dom";
 import { fetchServer } from "@common/dataFetch/fetchServer.js";
-import { useQuery, useMutation } from "@common/dataFetch/getQuery.js";
+
 import Button from "@common/components/Button.jsx";
 import openModal from "@common/modal/openModal.js";
-
 import AlertModal from "@admin/modals/AlertModal.jsx";
 
 import DrawResultModal from "./DrawResultModal.jsx";
@@ -12,55 +12,93 @@ import Suspense from "@common/components/Suspense.jsx";
 import Spinner from "@common/components/Spinner.jsx";
 import DelaySkeleton from "@common/components/DelaySkeleton.jsx";
 
+function ResultModalContainer({eventId})
+{
+  return <div className="w-[calc(100vw-8rem)] h-[calc(100vh-8rem)] p-8 bg-white relative">
+    <ErrorBoundary fallback={<div>에러남</div>}>
+      <Suspense
+        fallback={
+          <div className="w-full h-full flex justify-center items-center">
+            <DelaySkeleton>
+              <Spinner />
+            </DelaySkeleton>
+          </div>
+        }
+      >
+        <DrawResultModal eventId={eventId} />
+      </Suspense>
+    </ErrorBoundary>
+  </div>
+}
+
 function DrawButton() {
   const { eventId } = useParams();
-  const drawResultData = useQuery(
-    `event-detail-draw-result-${eventId}`,
-    () => {
-      return fetchServer(`/api/v1/admin/draw/${eventId}/winners`);
-    },
-    { deferred: true },
-  );
+  const [drawState, setDrawState] = useState("BEFORE_END");
+  const interval = useRef(null);
+  const timeout = useRef(null);
 
-  const resultModal = (
-    <div className="w-[calc(100vw-8rem)] h-[calc(100vh-8rem)] p-8 bg-white relative">
-      <ErrorBoundary fallback={<div>에러남</div>}>
-        <Suspense
-          fallback={
-            <div className="w-full h-full flex justify-center items-center">
-              <DelaySkeleton>
-                <Spinner />
-              </DelaySkeleton>
-            </div>
-          }
-        >
-          <DrawResultModal eventId={eventId} />
-        </Suspense>
-      </ErrorBoundary>
-    </div>
-  );
+  useEffect( ()=>{
+    fetchServer(`/api/v1/admin/draw/${eventId}/status`)
+      .then( ({status})=>setDrawState(status) )
+      .catch( (e)=>{
+        setDrawState("ERROR");
+        } );
+  }, [] );
 
-  const mutate = useMutation(
-    `event-detail-draw-result-${eventId}`,
-    () => fetchServer(`/api/v1/admin/draw/${eventId}/draw`, { method: "post" }),
+  useEffect( ()=>{
+    return ()=>{
+      clearInterval(interval.current);
+      clearTimeout(timeout.current);
+    }
+  }, [] );
+
+  async function onSubmit()
+  {
+    function shortPooling()
     {
-      onSuccess: () => openModal(resultModal),
-      onFail: () =>
-        openModal(<AlertModal title="오류" description="추첨에 오류가 발생했습니다." />),
-    },
-  );
+      fetchServer(`/api/v1/admin/draw/${eventId}/status`)
+        .then( ({status})=>{
+          setDrawState(status);
+          if(status !== "IS_DRAWING") {
+            clearInterval(interval.current);
+          }
+        } )
+        .catch( (e)=>{
+          setDrawState("ERROR");
+          clearInterval(interval.current);
+        } );
+    }
 
-  if (drawResultData.length === 0)
-    return (
-      <Button className="w-32 h-8 px-4 py-1" onClick={mutate}>
+    try {
+      await fetchServer(`/api/v1/admin/draw/${eventId}/draw`, { method: "post" });
+      setDrawState("IS_DRAWING");
+      openModal(<AlertModal title="성공" description="성공적으로 추첨 요청이 전송되었습니다." />);
+      timeout.current = setTimeout( ()=>{
+        shortPooling();
+        interval.current = setInterval( shortPooling, 5000 );
+      }, 500 );
+    }
+    catch {
+      openModal(<AlertModal title="오류" description="추첨에 오류가 발생했습니다." />);
+    }
+  }
+
+  switch(drawState)
+  {
+    case "BEFORE_END": return null;
+    case "AVAILABLE": return (
+      <Button className="w-32 h-8 px-4 py-1" onClick={onSubmit}>
         추첨하기
       </Button>
     );
-  return (
-    <Button className="w-32 h-8 px-4 py-1" onClick={() => openModal(resultModal)}>
-      결과 보기
-    </Button>
-  );
+    case "IS_DRAWING": return <div className="w-32 h-8 px-4 py-1 bg-neutral-600 text-neutral-400">추첨 진행중...</div>;
+    case "COMPLETE": return (
+      <Button className="w-32 h-8 px-4 py-1" onClick={() => openModal(<ResultModalContainer eventId={eventId} />)}>
+        결과 보기
+      </Button>
+    );
+    default: return <div className="w-32 h-8 px-4 py-1 bg-neutral-600 text-neutral-400">에러</div>;
+  }
 }
 
 export default DrawButton;
